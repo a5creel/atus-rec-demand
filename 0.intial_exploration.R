1a280da9707ab465877f9a45ea6794d2b7013eac
a5creel <andie.creel@gmail.com>
2022-09-30 15:31:43 -0400

    half way through reading in every year's summary file and cleaning each

--- 0.intial_exploration.R
+++ 0.intial_exploration.R
@@ -9,35 +9,108 @@
 # -----------------------------------------------------------------------------
 library(vroom)
 library(dplyr)
+library(data.table)
+
 
 
 # -----------------------------------------------------------------------------
 # reading in data 
-
-# The 2003-21 Activity Summary file contains data for the total number of minutes that each respondent spent doing each 6-digit activity.  
-# Each record corresponds to a unique respondent, as indicated by a unique value of the variable TUCASEID.  
-
-# The variable names identify activities by their 6-digit codes.  The letter "t" precedes the 6-digit codes.  The first and second digits 
-# of the 6-digit code correspond to the variable trtier1p; the first, second, third and fourth digits of the 6-digit code correspond to the 
-# variable trtier2p; and, all six digits of the 6-digit code correspond to the variable trcodep.  For example, the variable "t040102" 
-# refers to the activity with TRTIER1P=04, TRTIER2P=0401, and TRCODEP=040102.  As documented in the ATUS 2003-21 Coding Lexicon 
-# (https://www.bls.gov/tus/lexiconnoex0321.pdf), activity code "040102" corresponds to "Caring for and helping nonhousehold members/
-# Caring for and helping nonhousehold children/Reading to or with nonhousehold children." See Chapter 5 of the ATUS User's Guide 
-# (https://www.bls.gov/tus/atususersguide.pdf) for more information about the ATUS Coding Lexicon. 
-# 
-# In addition to the activity code variables (t010101...t509999), the file atussum_0321.dat includes selected variables (such as age, sex, 
-# and labor force status of the respondent) from the Respondent and ATUS-CPS files.  The definitions for variables that begin with the letter 
-# "P" or "G" appear in the 2003-21 ATUS-CPS Data Dictionary, available at https://www.bls.gov/tus/atuscpscodebk0321.pdf; the definitions 
-# for variables that begin with the letter "T" (other than t010101...t509999) appear in the 2003-21 ATUS Interview Data Dictionary, available at 
-# https://www.bls.gov/tus/atusintcodebk0321.pdf.  
 # -----------------------------------------------------------------------------
 
-myOG <- vroom('raw_data/atussum-0321/atussum_0321.dat')  
+# The 2021 Activity Summary file contains data for the total number of minutes 
+# that each respondent spent doing each 6-digit activity.  Each record 
+# corresponds to a unique respondent, as indicated by a unique value of the 
+# variable TUCASEID. 
 
-myWorking <- myOG %>%
+# -----------------------------------------------------------------------------
+# Reading in summary file for each year
+# -----------------------------------------------------------------------------
+
+# List to store all summary files ing 
+list_df <- list() 
+
+# 2003 - 2017
+for (k in 3:17){
+  file_name <- paste0("curl https://www.bls.gov/tus/special.requests/atussum_20",if_else(k<10, paste0("0", k), paste0(k)),".zip | tar -xf- --to-stdout *.dat")
+  
+  list_df[[k]] <- fread(file_name)
+}
+
+# 2018 - 2021
+for (k in 18:21){
+  file_name <- paste0("curl https://www.bls.gov/tus/special.requests/atussum-20",if_else(k<10, paste0("0", k), paste0(k)),".zip | tar -xf- --to-stdout *.dat")
+  
+  list_df[[k]] <- fread(file_name)
+}
+
+
+#NEED TO REDO THIS FOR DIFFERENT YEARS 
+
+myDataClean <- function(df, year){
+  
+  # if year is XXX then variables are XXXX 
+  
+  myW <- df %>%
+    rename(race = PTDTRACE) %>%
+    rename(week_earning = TRERNWA) %>%
+    #rename(household_id = TUCASEID) %>%
+    rename(sample_weight = TUFINLWGT) %>%
+    select(household_id, race, week_earning, sample_weight, starts_with("t1301")) %>% #Sports, Exercise, & Recreation
+    rowwise() %>% mutate(minutes_recreation = sum(c_across(cols = starts_with('t1301'))))
+  
+  myW
+}
+
+list_df_clear <- list()
+list_df_clear <- for (k in 3:21) {
+  list_df_clear[[k]] <-myDataClean(list_df[[k]])
+}
+
+myWorking_21 <- myOG_21 %>%
   rename(race = PTDTRACE) %>%
   rename(week_earning = TRERNWA) %>%
   rename(household_id = TUCASEID) %>%
-  select(household_id, race, week_earning, starts_with("t1301")) #Sports, Exercise, & Recreation
+  rename(sample_weight = TUFINLWGT) %>%
+  select(household_id, race, week_earning, sample_weight, starts_with("t1301")) %>% #Sports, Exercise, & Recreation
+  rowwise() %>% mutate(minutes_recreation = sum(c_across(cols = starts_with('t1301'))))
+
+# average 
+avg_min_rec_21 = sum(myWorking_21$sample_weight*myWorking_21$minutes_recreation)/sum(myWorking_21$sample_weight)
+
+# -----------------------------------------------------------------------------
+# 2003
+# -----------------------------------------------------------------------------
+
+myOG_03 <- vroom('raw_data/2003/atussum_2003/atussum_2003.dat')  
+
+myWorking_03 <- myOG_03 %>%
+  rename(race = PTDTRACE) %>%
+  rename(week_earning = TRERNWA) %>%
+  rename(household_id = TUCASEID) %>%
+  rename(sample_weight = TUFINLWGT) %>%
+  select(household_id, race, week_earning, sample_weight, starts_with("t1301")) %>% #Sports, Exercise, & Recreation
+  rowwise() %>% mutate(minutes_recreation = sum(c_across(cols = starts_with('t1301'))))
+
+# average 
+avg_min_rec_03 = sum(myWorking_03$sample_weight*myWorking_03$minutes_recreation)/sum(myWorking_03$sample_weight)
+
+unzip("https://www.bls.gov/tus/special.requests/atussum_2003.zip", list=TRUE)$Name
+
+library(data.table)
+
+list_df <- list() # creates a list
+for (k in 3:17){
+  file_name <- paste0("curl https://www.bls.gov/tus/special.requests/atussum_20",if_else(k<10, paste0("0", k), paste0(k)),".zip | tar -xf- --to-stdout *.dat")
+  
+  list_df[[k]] <- fread(file_name)
+}
+
+for (k in 18:21){
+  file_name <- paste0("curl https://www.bls.gov/tus/special.requests/atussum-20",if_else(k<10, paste0("0", k), paste0(k)),".zip | tar -xf- --to-stdout *.dat")
+  
+  list_df[[k]] <- fread(file_name)
+}
+
+
 
 
