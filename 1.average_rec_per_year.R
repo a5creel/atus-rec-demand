444bd7843ea800bab19590cf8121330530d51662
Andie Creel <a5creel@Andies-Air.wireless.yale.internal>
2022-10-07 16:59:02 -0400

    broke down by race

--- 1.average_rec_per_year.R
+++ 1.average_rec_per_year.R
@@ -40,20 +40,67 @@ get_avg_rec_min <- function(df){
 myYear <- 2003:2021
 myAverages <- data.frame(year = myYear, avg_min_per_day = NA)
 
-for (k in 3:21) {
-  myAverages$avg_min_per_day[k-2] <- get_avg_rec_min(myList[[k]])
+for (y in 3:21) {
+  myAverages$avg_min_per_day[y-2] <- get_avg_rec_min(myList[[y]])
+  
+  # -----------------------------------------------------------------------------
+  # break down by race
+  # 1 White only
+  # 2 Black only
+  # 3 American Indian, Alaskan Native only
+  # 4 Asian only
+  # 5 Hawaiian/Pacific Islander only 
+  # https://www.bls.gov/tus/atuscpscodebk14.pdf
+  
+  myRaceList <- list()
+  for (r in 1:5) {
+    myRaceList[[r]] <- myList[[y]] %>%
+      select(household_id, race, week_earning, sample_weight, total_min_rec) %>% 
+      filter(race == r)
+    
+    if (r == 1){
+      myAverages$avg_min_per_day_white[y-2] <- get_avg_rec_min(myRaceList[[r]])
+      
+    } else if (r == 2){
+      myAverages$avg_min_per_day_black[y-2] <- get_avg_rec_min(myRaceList[[r]])
+      
+    } else if (r == 3){
+      myAverages$avg_min_per_day_native[y-2] <- get_avg_rec_min(myRaceList[[r]])
+    } else if (r == 4){
+      myAverages$avg_min_per_day_asian[y-2] <- get_avg_rec_min(myRaceList[[r]])
+    } else if (r == 5){
+      myAverages$avg_min_per_day_island[y-2] <- get_avg_rec_min(myRaceList[[r]])
+    }
+    
+  }
+  # -----------------------------------------------------------------------------
+  
   
 }
 
+# -----------------------------------------------------------------------------
+# plot
+# -----------------------------------------------------------------------------
+
 myAverages %>%
-  ggplot(aes(x = year, y = avg_min_per_day)) +
-  geom_point(colour = "black") +
-  geom_smooth(method = "lm", fill = "grey", color = "darkred") +
-  ggtitle("Time spent outdoors may be increasing", "The avg. American saw a ~7% increase in outdoor recreation") +
+  ggplot(aes(x = year)) +
+  # geom_point(colour = "black") +
+  geom_smooth(aes(y = avg_min_per_day), method = "lm", fill = "grey", color = "darkred") +
+  geom_smooth(aes(y = avg_min_per_day_white), method = "lm", fill = NA, color = "darkblue") +
+  geom_smooth(aes(y = avg_min_per_day_black), method = "lm", fill = NA, color = "darkgreen") +
+  # geom_smooth(aes(y = avg_min_per_day_native), method = "lm", fill = NA, color = "purple") +
+  # geom_smooth(aes(y = avg_min_per_day_island), method = "lm", fill = NA, color = "coral") +
+  ggtitle("Time spent recreating varries by race", "The avg. American saw a ~7% increase in outdoor recreation") +
   ylab("avg min recreating outside per day") +
   xlab("Year")+
-  theme_bw()
-
+  labs(caption = "Data Source: ATUS Microdata Activity Summary files, 2003-2021")+
+  theme_bw() + 
+  scale_colour_manual("", 
+                      breaks = c("Population", "White", "Black"),
+                      values = c("darkred", "darkblue", "darkgreen"))
+# 
 summary(myAverages$avg_min_per_day)
 
 
+
+#NEED TO LABEL WHITE AND BLACK LINES!
